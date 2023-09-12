50bdbdca945244da271dcae33b3cbbb080c0d5a8
real-ADITYA <adityareswaran@gmail.com>
2023-09-12 14:05:43 -0400

    Created around Nov. 2018

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# CSharp-HS
+# My First Steps in C# (2018)
+
+Contents orginally created from August 2018 - January 2019
+
+
+
+## A bit about this repository.
+
+🚀 Welcome to my high school coding journey! This GitHub repository is a nostalgic collection of projects I embarked on in 2018, marking the beginning of my programming adventure. These projects were crafted using C#, the language that ignited my passion for computer science and led me to pursue it in college at the University of Maryland - College Park.
+
+## 💡 Why?
+
+These projects are not just a showcase of my early work but also a testament to the power of curiosity and dedication. Discover how my high school journey with C# shaped my thinking, cultivated my problem-solving abilities, and set me on the path to a Computer Science degree.
+
+
+
+
+## 📚 Looking Forward:
+
+I've come a long way since these projects, but they hold a special place in my heart as they represent the beginning of my coding odyssey. Explore them, learn from my early experiences, and join me on my ongoing journey through the world of software development.
+
+
+
+✉️ Contact:
+📧 Email: adityareswaran@gmail.com
+🌐 LinkedIn: [Your LinkedIn Profile]
+
+🙏 Thank you for visiting my GitHub repository and being a part of my coding evolution! 🚀👨‍💻
+
+
+
+🌟 Featured Projects:
+1️⃣ **Project A - The Game Changer**
+
+- An exciting game built from scratch in C# that was my very first coding endeavor. Witness my progress from a complete beginner to a budding developer.
+
+2️⃣ **Project B - Data-driven App**
+
+- A data-driven Windows application demonstrating my growing skills in C#. Explore how I leveraged the language to create an intuitive user interface and interact with databases.
+
+3️⃣ **Project C - Algorithmic Prowess**
+
+- Dive into the world of algorithms and problem-solving with this project. See how I utilized C# to tackle complex computational challenges and develop efficient solutions.
+
+💡 What's Special?
+📚 Looking Forward:
+I've come a long way since these projects, but they hold a special place in my heart as they represent the beginning of my coding odyssey. Explore them, learn from my early experiences, and join me on my ongoing journey through the world of software development.
+
+🔗 Connect with Me:
+Feel free to connect with me, share your thoughts, or ask questions about my coding journey. Let's continue to learn and grow together in the fascinating realm of computer science!
+
+✉️ Contact:
+📧 Email: [Your Email Address]
+🌐 LinkedIn: [Your LinkedIn Profile]
+🐦 Twitter: [@YourTwitterHandle]
+
+🙏 Thank you for visiting my GitHub repository and being a part of my coding evolution! 🚀👨‍💻 #CSJourney #CSharp #HighSchoolCoding
---
+++ TaxRelatedMethodPrblms/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace TaxRelatedMethodPrblms {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("-----------TAX CALCULATOR-----------\nWhat is your annual income (Please do not include a comma): ");
+            double userIncome = Convert.ToDouble(Console.ReadLine());
+            float taxAmnt = HowMuchDoIOwe(TaxBracket(userIncome), userIncome);
+            Console.WriteLine("You owe " + taxAmnt + " in taxes this year LOL.");
+
+
+            static int TaxBracket(double userIncome1) {
+                int whatBracket;
+                if (userIncome1 >= 0 && userIncome1 <= 9525) {
+                    whatBracket = 0;
+                    Console.WriteLine("You earned " + userIncome1 + " this year, so you are in the $0 to $9,525 tax bracket.");
+                    return whatBracket;
+                }
+                else if (userIncome1 >= 9526 && userIncome1 <= 38700) {
+                    whatBracket = 1;
+                    Console.WriteLine("You earned " + userIncome1 + " this year, so you are in the $9,526 to $38,700 tax bracket.");
+                    return whatBracket;
+                }
+                else if (userIncome1 >= 38701 && userIncome1 <= 82500) {
+                    whatBracket = 2;
+                    Console.WriteLine("You earned " + userIncome1 + " this year, so you are in the $38,701 to $82,500 tax bracket.");
+                    return whatBracket;
+                }
+                else if (userIncome1 >= 82501 && userIncome1 <= 157500) {
+                    whatBracket = 3;
+                    Console.WriteLine("You earned " + userIncome1 + " this year, so you are in the $82,501 to $15,7500 tax bracket.");
+                    return whatBracket;
+                }
+                else if (userIncome1 >= 159501 && userIncome1 <= 200000) {
+                    whatBracket = 4;
+                    Console.WriteLine("You earned " + userIncome1 + " this year, so you are in the $15,7501 to $200,000 tax bracket.");
+                    return whatBracket;
+                }
+                else if (userIncome1 >= 200001 && userIncome1 <= 500000) {
+                    whatBracket = 5;
+                    Console.WriteLine("You earned " + userIncome1 + " this year, so you are in the $200,001 to $500,000 tax bracket.");
+                    return whatBracket;
+                }
+                else if (userIncome1 >= 500001) {
+                    whatBracket = 6;
+                    Console.WriteLine("You earned " + userIncome1 + " this year, so you are in the $500,001 or higher tax bracket.");
+                    return whatBracket;
+                }
+                return whatBracket = -1;
+            }
+
+
+            static float HowMuchDoIOwe(int bracketNumID, double userIncome2) {
+                float userTaxMoney;
+                float userIncome3 = Convert.ToSingle(userIncome2);
+                switch (bracketNumID) {
+                    case (0):
+                        userTaxMoney = userIncome3 * 0.1f;
+                        return userTaxMoney;
+                    case (1):
+                        userTaxMoney = 952.50f + ((userIncome3 - 9525f) * 0.12f);
+                        return userTaxMoney;
+                    case (2):
+                        userTaxMoney = 4453.50f + ((userIncome3 - 38700f) * 0.22f);
+                        return userTaxMoney;
+                    case (3):
+                        userTaxMoney = 14089.50f + ((userIncome3 - 82500f) * 0.24f);
+                        return userTaxMoney;
+                    case (4):
+                        userTaxMoney = 32089.50f + ((userIncome3 - 157500f) * 0.32f);
+                        return userTaxMoney;
+                    case (5):
+                        userTaxMoney = 45689.50f + ((userIncome3 - 200000f) * 0.35f);
+                        return userTaxMoney;
+                    case (6):
+                        userTaxMoney = 150689.50f + ((userIncome3 - 500000f) * 0.37f);
+                        return userTaxMoney;
+                }
+                return userTaxMoney = 0;
+            }
+
+        }
+    }
+}
